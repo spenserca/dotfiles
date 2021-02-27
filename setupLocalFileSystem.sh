7c1eaf3afc559eb451d55dd13dd968c7e2d93a8c
Spenser Andreassen <spenserca@gmail.com>
2021-02-27 10:15:54 -0600

    make directory structure before linking files

--- setupLocalFileSystem.sh
+++ setupLocalFileSystem.sh
@@ -36,21 +36,6 @@ link_file () {
     fi
 }
 
-# setup bash files
-
-BASH_ALIASES_DESTINATION=~/.bash_aliases
-BASH_ALIASES_SOURCE=~/repos/personal/dotfiles/bash/.bash_aliases
-BASH_PROFILE_DESTINATION=~/.bash_profile
-BASH_PROFILE_SOURCE=~/repos/personal/dotfiles/bash/.bash_profile
-BASHRC_DESTINATION=~/.bashrc
-BASHRC_SOURCE=~/repos/personal/dotfiles/bash/.bashrc
-
-create_empty_file ~/.local_aliases
-create_empty_file ~/local_bashrc
-link_file $BASH_ALIASES_SOURCE $BASH_ALIASES_DESTINATION
-link_file $BASH_PROFILE_SOURCE $BASH_PROFILE_DESTINATION
-link_file $BASHRC_SOURCE $BASHRC_DESTINATION
-
 # setup notes structure
 
 create_directory ~/notes
@@ -70,3 +55,18 @@ create_directory $XILUTION_DIR
 CASEYS_DIR=$REPOS_DIR/caseys
 
 create_directory $CASEYS_DIR
+
+# setup bash files
+
+BASH_ALIASES_DESTINATION=~/.bash_aliases
+BASH_ALIASES_SOURCE=~/repos/personal/dotfiles/bash/.bash_aliases
+BASH_PROFILE_DESTINATION=~/.bash_profile
+BASH_PROFILE_SOURCE=~/repos/personal/dotfiles/bash/.bash_profile
+BASHRC_DESTINATION=~/.bashrc
+BASHRC_SOURCE=~/repos/personal/dotfiles/bash/.bashrc
+
+create_empty_file ~/.local_aliases
+create_empty_file ~/local_bashrc
+link_file $BASH_ALIASES_SOURCE $BASH_ALIASES_DESTINATION
+link_file $BASH_PROFILE_SOURCE $BASH_PROFILE_DESTINATION
+link_file $BASHRC_SOURCE $BASHRC_DESTINATION
