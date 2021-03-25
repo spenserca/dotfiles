58730f57eac4ebbea814294b3da2fe2f9cf6cbe0
Spenser Andreassen <spenserca@gmail.com>
2021-03-24 20:24:10 -0500

    add casks installs and line breaks

--- mac/installHomebrew.sh
+++ mac/installHomebrew.sh
@@ -1,7 +1,12 @@
 GREEN='\033[2;32m'
+YELLOW='\033[2;33m'
 
 log() {
   echo "${GREEN}$1"
+}
+
+linebreak() {
+  echo "${YELLOW}--*--*--*--*--"
 }
 
 install() {
@@ -13,8 +18,25 @@ install() {
     log "Installing $BREW_PACKAGE"
     brew install $BREW_PACKAGE
   else
-    log "$BREW_PACKAGE is already installed -- skipping installation"
+    log "$BREW_PACKAGE is already installed -- using $($TEST_COMMAND --version)"
   fi
+
+  linebreak
+}
+
+install_cask() {
+  TEST_COMMAND=$1
+  BREW_PACKAGE=$2
+
+  if test ! $(which $TEST_COMMAND)
+  then
+    log "Installing $BREW_PACKAGE"
+    brew install --cask $BREW_PACKAGE
+  else
+    log "$BREW_PACKAGE is already installed -- using $($TEST_COMMAND --version)"
+  fi
+
+  linebreak
 }
 
 if test ! $(which brew)
@@ -26,12 +48,15 @@ else
 fi
 
 log "Using Homebrew version $(brew --version)"
+linebreak
 
 log "Updating any existing recipes"
 brew update
+linebreak
 
 log "Upgrading any existing formulae"
 brew upgrade
+linebreak
 
 # You can find mac formulae here: https://formulae.brew.sh/formula/
 
@@ -40,3 +65,7 @@ install node nodejs
 install az azurecli
 
 install aws awscli
+
+install terraform terraform
+
+install_cask code visual-studio-code
