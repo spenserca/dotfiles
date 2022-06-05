174bbdf4eec1b7b96f24c7a83a9d2a5cf444018c
Spenser Andreassen <spenserca@gmail.com>
2022-06-05 14:20:53 -0500

    git config working

--- git/setup.sh
+++ git/setup.sh
@@ -4,6 +4,9 @@ source ./functions.sh
 
 GIT_CONFIG_FILE=.gitconfig
 WORK_GIT_CONFIG_FILE=.gitconfig-work
+PERSONAL_GIT_CONFIG_FILE=.gitconfig-personal
 
 copy_file ./git/$GIT_CONFIG_FILE ~/$GIT_CONFIG_FILE
+# TODO: take work email/name in as parameters and inject into config
 copy_file ./git/$WORK_GIT_CONFIG_FILE ~/$WORK_GIT_CONFIG_FILE
+copy_file ./git/$PERSONAL_GIT_CONFIG_FILE ~/$PERSONAL_GIT_CONFIG_FILE
