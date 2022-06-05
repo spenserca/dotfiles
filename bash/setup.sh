cc366949c561c0123d8f5f1c7c1aab1f6ccb85ce
Spenser Andreassen <spenserca@gmail.com>
2022-06-05 10:55:05 -0500

    refactor: loop over files

--- bash/setup.sh
+++ bash/setup.sh
@@ -2,21 +2,19 @@
 
 source ./functions.sh
 
-BASH_ALIASES_SOURCE=./bash/.bash_aliases
-BASH_ALIASES_DESTINATION=~/.bash_aliases
+FILES_TO_COPY=(
+  .bash_aliases
+  .bash_functions
+  .bash_profile
+  .bashrc
+)
 
-BASH_FUNCTIONS_SOURCE=./bash/.bash_functions
-BASH_FUNCTIONS_DESTINATION=~/.bash_functions
+for FILE in ${FILES_TO_COPY[@]}; do
+  copy_file ./bash/$FILE ~/$FILE
+done
 
-BASH_PROFILE_SOURCE=./bash/.bash_profile
-BASH_PROFILE_DESTINATION=~/.bash_profile
+EMPTY_FILES=(~/.local_aliases ~/.local_bashrc)
 
-BASHRC_SOURCE=./bash/.bashrc
-BASHRC_DESTINATION=~/.bashrc
-
-create_empty_file ~/.local_aliases
-create_empty_file ~/.local_bashrc
-copy_file $BASH_ALIASES_SOURCE $BASH_ALIASES_DESTINATION
-copy_file $BASH_FUNCTIONS_SOURCE $BASH_FUNCTIONS_DESTINATION
-copy_file $BASH_PROFILE_SOURCE $BASH_PROFILE_DESTINATION
-copy_file $BASHRC_SOURCE $BASHRC_DESTINATION
+for FILE in ${EMPTY_FILES[@]}; do
+  create_empty_file $FILE
+done
